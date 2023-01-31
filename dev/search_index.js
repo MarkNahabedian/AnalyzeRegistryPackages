1d5164cca01d5dd1d3adb71e27f79a6ac4aa2e1a
Documenter.jl <documenter@juliadocs.github.io>
2023-01-31 15:09:58 +0000

    build based on e33c4f6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AnalyzeRegistryPackages","category":"page"},{"location":"#AnalyzeRegistryPackages","page":"Home","title":"AnalyzeRegistryPackages","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AnalyzeRegistryPackages.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AnalyzeRegistryPackages]","category":"page"}]
+}
